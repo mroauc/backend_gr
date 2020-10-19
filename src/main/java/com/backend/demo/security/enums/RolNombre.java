e936fbf1fbdbd55798187175482381371f243baf
marceloR <marceloroa19@gmail.com>
2020-10-19 12:13:51 -0300

    Implementacion roles y login

---
+++ src/main/java/com/backend/demo/security/enums/RolNombre.java
@@ -0,0 +1,5 @@
+package com.backend.demo.security.enums;
+
+public enum RolNombre {
+    ROLE_ADMIN, ROLE_ANALISTA, ROLE_LIDER_SUBPROYECTO, ROLE_JEFE_PROYECTO, ROLE_CLIENTE
+}
---
+++ src/main/java/com/backend/demo/security/service/UsuarioService.java
@@ -0,0 +1,29 @@
+package com.backend.demo.security.service;
+
+import com.backend.demo.security.entity.Usuario;
+import com.backend.demo.security.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+public class UsuarioService {
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    public Optional<Usuario> getByEmail(String email){
+        return usuarioRepository.findByEmail(email);
+    }
+
+    public boolean existsByEmail(String email){
+        return usuarioRepository.existsByEmail(email);
+    }
+
+    public void save(Usuario usuario){
+        usuarioRepository.save(usuario);
+    }
+}
