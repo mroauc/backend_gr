662003f7c49704efd02b80cf7122983f9c73c4a6
marceloR <marceloroa19@gmail.com>
2020-11-02 11:11:38 -0300

    cambios usuario_rol

--- src/main/java/com/backend/demo/repositorio/RSubProyecto.java
+++ src/main/java/com/backend/demo/repositorio/RSubProyecto.java
@@ -6,8 +6,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.backend.demo.modelo.SubProyecto;
+///import com.backend.demo.modelo.UsuarioRol;
 
 public interface RSubProyecto extends CrudRepository<SubProyecto, Integer> {
 	@Query("SELECT s FROM SubProyecto s WHERE s.id_proyecto = :id_proyecto")
 	List<SubProyecto> findByid_proyecto(Integer id_proyecto);
+	
+	//@Query("SELECT r FROM usuario_rol r")
+	//List<UsuarioRol> UsuariosRoles();
 }
