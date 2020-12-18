48283e460a6e86b8f2b91dd83705594b34d58255
marceloR <marceloroa19@gmail.com>
2020-12-18 10:52:53 -0300

    nueva funcion

--- src/main/java/com/backend/demo/controlador/ClienteController.java
+++ src/main/java/com/backend/demo/controlador/ClienteController.java
@@ -1,5 +1,6 @@
 package com.backend.demo.controlador;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,7 +15,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.backend.demo.modelo.Cliente;
+import com.backend.demo.modelo.Empresa;
+import com.backend.demo.modelo.ProyectoEmpresa;
+import com.backend.demo.modelo.Seccion;
 import com.backend.demo.repositorio.RCliente;
+import com.backend.demo.repositorio.REmpresa;
+import com.backend.demo.repositorio.RProyectoEmpresa;
+import com.backend.demo.security.entity.Usuario;
+import com.backend.demo.security.repository.UsuarioRepository;
 
 @RestController
 @RequestMapping("/api/cliente")
@@ -23,6 +31,11 @@ public class ClienteController {
 	
 	@Autowired
 	private RCliente data;
+	@Autowired
+	private RProyectoEmpresa rProyectoEmpresa;
+	@Autowired
+	private UsuarioRepository rUsuario;
+	
 
 	@GetMapping("/")
 	public List<Cliente> listar(){
@@ -53,6 +66,34 @@ public class ClienteController {
 	@GetMapping("id_usuario/{id_usuario}")
 	public Optional<Cliente> ClienteByIdUsuario(@PathVariable int id_usuario){
 		return data.getClienteByIdUsuario(id_usuario);
+	}
+	
+	@GetMapping("/id_proyecto/{id_proyecto}")
+	public List<Usuario> ClienteByIdProyecto(@PathVariable int id_proyecto){
+		
+		List<Cliente> ClientesActuales = this.listar();
+		List<Integer> idClientesFiltrados = new ArrayList<Integer>();
+		List<Usuario> usuariosFinales = new ArrayList<Usuario>();
+		List<Integer> idEmpresas = new ArrayList<Integer>();
+		List<ProyectoEmpresa> proyecto_empresas = rProyectoEmpresa.findAllByid_proyecto(id_proyecto);
+		
+		for (ProyectoEmpresa item: proyecto_empresas) {
+			idEmpresas.add(item.getId_empresa());
+		}
+		
+		for (Cliente item: ClientesActuales) {
+			if(idEmpresas.indexOf(item.getId_empresa()) != -1) {
+				idClientesFiltrados.add(item.getId_user());
+			}
+		}
+		
+		for (Integer item : idClientesFiltrados) {
+			usuariosFinales.add(rUsuario.findById(item).get());
+		}
+		
+		
+		return usuariosFinales;
+		
 	}
 	
 	
--- src/main/java/com/backend/demo/repositorio/REmpresa.java
+++ src/main/java/com/backend/demo/repositorio/REmpresa.java
@@ -1,9 +1,11 @@
 package com.backend.demo.repositorio;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.backend.demo.modelo.Empresa;
 
 public interface REmpresa extends CrudRepository<Empresa, Integer>{
-
 }
